d502da0c8e0ff2e0db36be5d024c9bd536bd9ad3
josafa <josafaverissimo98@gmail.com>
2021-11-19 01:45:06 -0300

    main call interface indicated

--- hltv/src/main.py
+++ hltv/src/main.py
@@ -1,6 +1,18 @@
+from sys import argv
 from app.user_interface.teams_stats import TeamsStats
 from app.user_interface.get_team import GetTeam
 
-team_stats_screen = TeamsStats()
+def show_screen(screen):
+    screen_by_module = {
+        'team_stats': TeamsStats().screen,
+        'get_team': GetTeam().screen
+    }
 
-team_stats_screen.screen()
+    if screen in screen_by_module:
+        screen_by_module[screen]()
+    else:
+        print('screen inexistent')
+
+screen = argv[1]
+
+show_screen(screen)
