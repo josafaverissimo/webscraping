9a8f28d8137b3df2006c45b36749c08f34e5f4a2
josafa <josafaverissimo98@gmail.com>
2021-12-11 16:26:40 -0300

    relationships added

--- hltv/src/app/utils/database/orms/match_team_map_result.py
+++ hltv/src/app/utils/database/orms/match_team_map_result.py
@@ -1,4 +1,7 @@
 from .orm import Orm
+from .map import Map
+from .team import Team
+from .match import Match
 
 
 class MatchTeamMapResult(Orm):
@@ -8,7 +11,8 @@ class MatchTeamMapResult(Orm):
         map_id=None,
         match_id=None,
         ct_rounds_wins=None,
-        tr_rounds_wins=None
+        tr_rounds_wins=None,
+        relationships=None
     ):
         table_name = 'matches_teams_maps_results'
         columns = {
@@ -29,4 +33,10 @@ class MatchTeamMapResult(Orm):
             'tr_rounds_wins': int
         }
 
-        super().__init__(table_name, columns, get_columns, set_columns)
+        relationships_by_table_name = Orm.get_orms_by_relationships(relationships, {
+            'maps': {'references_key': 'id', 'foreign_key': 'map_id', 'orm': Map},
+            'teams': {'references_key': 'id', 'foreign_key': 'team_id', 'orm': Team},
+            'matches': {'references_key': 'id', 'foreign_key': 'match_id', 'orm': Match},
+        })
+
+        super().__init__(table_name, columns, get_columns, set_columns, relationships_by_table_name)
